c834eba40ae634df7684e9875a3825b489431621
GalinStoychev <stoychev.galin@gmail.com>
2016-09-20 20:14:24 +0300

    delete old test methods

--- app/app.js
+++ app/app.js
@@ -9,21 +9,6 @@ let app = express();
 app.use(bodyParser.json());
 app.use(express.static('./'));
 
-app.get('/items', function (req, res) {
-    let items = db.getItems();
-    res.json(items);
-});
-
-
-app.post('/items', function (req, res) {
-    var item = req.body;
-    console.log(req.body);
-    db.addToStorage(item).then((item) => {
-        res.json(item);
-    }, function (err) {
-        res.status(400).json(err);
-    });
-});
 
 
 
--- db/db.js
+++ db/db.js
@@ -1,17 +1,1 @@
 'use strinct';
-let storage = [];
-
-function addToStorage(item) {
-    return new Promise((resolve, reject) => {
-        storage.push(item);
-        resolve(item);
-    });
-}
-
-function getItems() {
-    return storage;
-}
-
-module.exports = {
-    addToStorage, getItems
-};
