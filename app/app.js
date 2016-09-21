6fae47a967e38680b204f055351a82c1fdfe1cf6
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-09-22 00:01:56 +0300

    * Added Lightweight Database ET * Added gitignore

--- app/app.js
+++ app/app.js
@@ -1,19 +1,31 @@
 'use strict';
-let db = require('../db/db');
-
 let express = require('express'),
+    db = require('../db/db'),
     bodyParser = require('body-parser');
 
 let app = express();
-
 app.use(bodyParser.json());
 app.use(express.static('./'));
-
-
-
 
 var port = 3333;
 app.listen(port, function () {
     console.log(`Server is running at http://localhost:${port}`);
 });
 
+
+/* DB TEST just to check if working _ > should be deleted */
+var doc = { hello: 'world'
+    , n: 10
+    , today: new Date()
+    , nedbIsAwesome: true
+    , notthere: null
+    , notToBeSaved: undefined  // Will not be saved
+    , fruits: [ 'apple', 'orange', 'pear' ]
+    , infos: { name: 'nedb' }
+};
+
+db.users.insert(doc, function (err, newDoc) {});
+
+db.users.find({ nedbIsAwesome: true }, function (err, docs) {
+    console.dir(docs);
+});
--- db/db.js
+++ db/db.js
@@ -1,1 +1,10 @@
-'use strinct';
+var Datastore = require('nedb');
+
+var db = {};
+
+db.users = new Datastore({ filename: 'db/models/users.db', autoload: true });
+db.categories = new Datastore({ filename: 'db/models/categories.db', autoload: true });
+db.books= new Datastore({ filename: 'db/models/books.db', autoload: true });
+db.orders = new Datastore({ filename: 'db/models/orders.db', autoload: true });
+
+module.exports = db;
