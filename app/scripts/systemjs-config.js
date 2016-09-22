58f4466d33adc5796401deba3882393c47058093
SHAMMY1 <shammy.bg1@gmail.com>
2016-09-23 00:40:02 +0300

    base single page functionality -> sammy handlebars

--- app/scripts/systemjs-config.js
+++ app/scripts/systemjs-config.js
@@ -7,6 +7,9 @@ SystemJS.config({
         'data': './app/scripts/data.js',
         'jquery': './node_modules/jquery/dist/jquery.js',
         'bootstrap-utils': './public/bower_components/bootstrap/js/dist/util.js',
-        'bootstrap-tabs': './public/bower_components/bootstrap/js/dist/tab.js'
+        'bootstrap-tabs': './public/bower_components/bootstrap/js/dist/tab.js',
+        'sammy': './public/bower_components/sammy/lib/min/sammy-latest.min.js',
+        'handlebars': './public/bower_components/handlebars/handlebars.min.js',
+        'template': './app/scripts/handlebars-template.js'
     }
 });
