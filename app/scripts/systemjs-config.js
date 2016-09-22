e80474cd1c9b5472f4f915b7bcbf560015a577ba
SHAMMY1 <shammy.bg1@gmail.com>
2016-09-22 22:53:29 +0300

    testt

--- app/scripts/systemjs-config.js
+++ app/scripts/systemjs-config.js
@@ -1,10 +1,10 @@
 SystemJS.config({
     'transpiler': 'plugin-babel',
     'map': {
-        'plugin-babel': 'node_modules/systemjs-plugin-babel/plugin-babel.js',
-        'systemjs-babel-build': '../../node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+        'plugin-babel': './node_modules/systemjs-plugin-babel/plugin-babel.js',
+        'systemjs-babel-build': './node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
         'main': './app/scripts/main.js',
         'data': './app/scripts/data.js',
-        'jquery': '../../node_modules/jquery/dist/jquery.js',
+        'jquery': './node_modules/jquery/dist/jquery.js',
     }
 });
