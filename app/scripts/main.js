ec5161634d08f967ff86e0c2ed270f345ad68360
SHAMMY1 <shammy.bg1@gmail.com>
2016-09-22 22:55:40 +0300

    fix paths for gitraw

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 
 import'jquery'
-import "/public/bower_components/bootstrap/js/dist/util.js";
-import "/public/bower_components/bootstrap/js/dist/tab.js";
+import "./public/bower_components/bootstrap/js/dist/util.js";
+import "./public/bower_components/bootstrap/js/dist/tab.js";
 
