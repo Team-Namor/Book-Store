a4634d651f71079263f501f22a708c68c4107cee
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-09-25 16:42:27 +0300

    Added MVC folders and user blueprint

---
+++ app/controllers/userController.js
@@ -0,0 +1,23 @@
+let db = rootRequire('db/db');
+
+export class UserController {
+    index() {
+
+    }
+
+    get() {
+
+    }
+
+    add() {
+
+    }
+
+    edit() {
+
+    }
+
+    delete() {
+
+    }
+}
---
+++ app/models/user.js
@@ -0,0 +1,40 @@
+export class User {
+    constructor(firstName, lastName, gender, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    get firstName() {
+        return this._firstName;
+    }
+
+    set firstName(value) {
+        this._firstName = value;
+    }
+
+    get lastName() {
+        return this._lastName;
+    }
+
+    set lastName(value) {
+        this._lastName = value;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(value) {
+        this._gender = value;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        this._age = value;
+    }
+}
