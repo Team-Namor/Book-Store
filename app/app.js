ac09e65597c450ceb8cee60a037ae1e398edc3a6
Dimitar <dimitar.kalaydzhievv@gmail.com>
2016-09-26 20:04:29 +0300

    Added registration endpoint

--- app/app.js
+++ app/app.js
@@ -13,3 +13,17 @@ var port = 3333;
 app.listen(port, function () {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+/* Backend Router */
+app.post('/register', function (req, res) {
+    var newUser = {
+        firstname: userData['firstname'],
+        lastname: userData['lastname'],
+        email: userData['email'],
+        password: userData['password']
+    };
+    
+    db.users.insert(newUser, function (err, newUser) {});
+
+    res.send('POST request to the homepage');
+});
--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,40 +1,26 @@
-let db = require('./database');
-
 class UserController {
     index() {
-       /* let users = {};
-
-        db.users.find({}, function (err, usersData) {
-            users = usersData;
-        });*/
-
-        //show view
+      
     }
 
     get(id) {
-       /* let user = {}
 
-        db.users.find({ id: id }, function (err, userFound) {
-            user = userFound;
-        });*/
-
-        //show view
     }
 
     add(userData) {
-        console.dir(userData);
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: '/register',
+                contentType: 'application/json',
+                method: 'POST',
+                data: JSON.stringify(userData)
+            })
+            .done(resolve)
+            .fail(reject)
+        });
+        
+        //return view
 
-        //todo validation
-
-        var newUser = {
-            firstname: userData['firstname'],
-            lastname: userData['lastname'],
-            email: userData['email'],
-            password: userData['password']
-        };
-
-
-        // db.users.insert(newUser, function (err, newUser) {});
     }
 
     edit() {
